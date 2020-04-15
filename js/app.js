553d6b28483fbb0ffa1a0100e20ca0d3463aba00
evitalinuxera <riverplatense@gmail.com>
2020-04-15 14:36:02 -0300

    clase del martes a las 15:00

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('Hola');
+//console.log('Hola');
+
+//console.log (nombre);
+
+
+//console.log("Este es nuestro primer programa de javascript");
+//console.log("Este es nuestro segundo programa de javascript");
+nombreUsuario = prompt("Ingrese su nombre: ");
+console.log("su nombre es " + nombreUsuario);
+edad = prompt("Cuántos años tenés?: ");
+console.log("Tenés " + edad + "años");
